ac70f6feb352477decd320c4b6c196e904c5b744
chajiuqqq <chajiuqqq@outlook.com>
2020-05-29 09:44:09 +0800

    first commit

---
+++ src/main/java/com/chajiu/bangbangbe/BangbangBeApplication.java
@@ -0,0 +1,13 @@
+package com.chajiu.bangbangbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BangbangBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BangbangBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chajiu/bangbangbe/BangbangBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chajiu.bangbangbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BangbangBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
